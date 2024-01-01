db5a5d58010e09be85fb76c8a4eb3ad529f59d27
Michael Gerhold <michael.gerhold@web.de>
2024-01-01 21:17:03 +0100

    removed commented-out code

--- src/net/sandbox_client.cpp
+++ src/net/sandbox_client.cpp
@@ -25,46 +25,5 @@ after_loop:
     std::cerr << text.length() << '\n' << text << '\n';
     socket.send({ std::byte{ '!' } }).wait();
     std::cerr << "sent confirmation\n";
-
-#if 0
-    using namespace std::string_view_literals;
-    static constexpr auto to_send = "this is a test"sv;
-    if (auto const num_bytes_sent = send(connect_socket, to_send.data(), static_cast<int>(to_send.length()), 0);
-        num_bytes_sent == SOCKET_ERROR) {
-        closesocket(connect_socket);
-        throw std::runtime_error{ "unable to send data" };
-    } else {
-        std::cout << "bytes sent: " << num_bytes_sent << '\n';
-    }
-
-    if (shutdown(connect_socket, SD_SEND) == SOCKET_ERROR) {
-        closesocket(connect_socket);
-        throw std::runtime_error{ std::format("shutdown failed: {}", WSAGetLastError()) };
-    }
-
-    while (true) {
-        auto receive_buffer = std::array<char, 512>{};
-        auto const num_bytes_received =
-                recv(connect_socket, receive_buffer.data(), static_cast<int>(receive_buffer.size()), 0);
-        if (num_bytes_received > 0) {
-            assert(num_bytes_received <= receive_buffer.size());
-            std::cout << "bytes received: " << num_bytes_received << '\n';
-            for (std::size_t i = 0; i < num_bytes_received; ++i) {
-                std::cout << receive_buffer.at(i);
-            }
-            std::cout << '\n';
-        } else if (num_bytes_received == 0) {
-            std::cout << "connection closed\n";
-            break;
-        } else {
-            std::cerr << "receive failed: " << WSAGetLastError() << '\n';
-            break;
-        }
-    };
-
-    closesocket(connect_socket);
-    std::cout << "socket closed\n";
-
-#endif
     std::cout << "successful program termination\n";
 }
--- src/net/sandbox_server.cpp
+++ src/net/sandbox_server.cpp
@@ -44,47 +44,5 @@ void run_sandbox_server() {
         std::this_thread::sleep_for(std::chrono::milliseconds(20));
     }
     std::cerr << "ending program...\n";
-
-#if 0
-    if (listen(listen_socket, SOMAXCONN) == SOCKET_ERROR) {
-        closesocket(listen_socket);
-        throw std::runtime_error{ std::format("failed to listen on socket: {}", WSAGetLastError()) };
-    }
-
-    auto const client_socket = accept(listen_socket, nullptr, nullptr);
-    if (client_socket == INVALID_SOCKET) {
-        closesocket(listen_socket);
-        throw std::runtime_error{ std::format("accepting client socket failed: {}", WSAGetLastError()) };
-    }
-
-    while (true) {
-        auto receive_buffer = std::array<char, 512>{};
-        if (auto const num_bytes_received =
-                    recv(client_socket, receive_buffer.data(), static_cast<int>(receive_buffer.size()), 0);
-            num_bytes_received > 0) {
-            std::cout << "bytes received: " << num_bytes_received << '\n';
-            for (std::size_t i = 0; i < num_bytes_received; ++i) {
-                std::cout << receive_buffer.at(i);
-            }
-            std::cout << '\n';
-
-            if (auto const num_bytes_sent = send(client_socket, receive_buffer.data(), num_bytes_received, 0);
-                num_bytes_sent == SOCKET_ERROR) {
-                std::cerr << "echoing back to the client failed: " << WSAGetLastError() << '\n';
-                closesocket(client_socket);
-                break;
-            } else {
-                std::cout << "bytes sent: " << num_bytes_sent << '\n';
-            }
-        } else if (num_bytes_received == 0) {
-            std::cout << "connection closing...\n";
-            break;
-        } else {
-            std::cerr << "receiving data from client failed: " << WSAGetLastError() << '\n';
-            break;
-        }
-    }
-    closesocket(client_socket);
-#endif
     std::cout << "successful program termination\n";
 }
