c4e538eae3a518debbf0c3cc0bd799425a112084
Michael Gerhold <michael.gerhold@web.de>
2024-01-02 21:06:51 +0100

    renamed project, cleanup

--- examples/client.cpp
+++ examples/client.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 #include <sockets/socket_lib.hpp>
 
-namespace c2k {
-    void run_sandbox_client() {
-        auto socket = SocketLib::create_client_socket(AddressFamily::Unspecified, "localhost", 12345);
+static void run_sandbox_client() {
+    using namespace c2k;
+    auto socket = SocketLib::create_client_socket(AddressFamily::Unspecified, "localhost", 12345);
 
-        auto buffer = std::string{};
-        while (socket.is_connected()) {
-            buffer += socket.receive_string(512).get();
-            auto const newline_position = buffer.find('\n');
-            if (newline_position != std::string::npos) {
-                std::cout << buffer.substr(0, newline_position) << '\n'; // contains newline
-                buffer.erase(0, newline_position + 1);
-            }
+    auto buffer = std::string{};
+    while (socket.is_connected()) {
+        buffer += socket.receive_string(512).get();
+        auto const newline_position = buffer.find('\n');
+        if (newline_position != std::string::npos) {
+            std::cout << buffer.substr(0, newline_position) << '\n'; // contains newline
+            buffer.erase(0, newline_position + 1);
         }
     }
-} // namespace c2k
+}
 
-int main() try { c2k::run_sandbox_client(); } catch (std::runtime_error const& e) {
+int main() try { run_sandbox_client(); } catch (std::runtime_error const& e) {
     std::cerr << "execution terminated unexpectedly: " << e.what();
 }
