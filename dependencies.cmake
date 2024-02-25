58a3849483710fdab42e5f980ba080581392544f
Michael Gerhold <michael.gerhold@web.de>
2024-02-25 21:37:18 +0100

    removed `tl::expected` dependency in favor of `std::expected`

--- dependencies.cmake
+++ dependencies.cmake
@@ -11,14 +11,5 @@ function(c2k_sockets_setup_dependencies)
                 "BUILD_GMOCK OFF"
                 "INSTALL_GTEST OFF"
         )
-        CPMAddPackage(
-                NAME TL_EXPECTED
-                GITHUB_REPOSITORY TartanLlama/expected
-                VERSION 1.1.0
-                OPTIONS
-                "EXPECTED_BUILD_PACKAGE OFF"
-                "EXPECTED_BUILD_TESTS OFF"
-                "EXPECTED_BUILD_PACKAGE_DEB OFF"
-        )
     endif ()
 endfunction()
