ef4f2ecc890514a5dfbce7a34c0fce764c489d0f
Michael Gerhold <michael.gerhold@web.de>
2024-03-16 16:11:31 +0100

    Rename variable for clarity in client example

--- examples/client.cpp
+++ examples/client.cpp
@@ -5,10 +5,10 @@ static void run_sandbox_client() {
     using namespace c2k;
     auto socket = Sockets::create_client(AddressFamily::Unspecified, "localhost", 12345);
     std::cout << "connected to server at " << socket.remote_address() << '\n';
-    auto extractor = MessageBuffer{};
+    auto buffer = MessageBuffer{};
     while (socket.is_connected()) {
-        extractor << socket.receive(512).get();
-        while (auto package = extractor.try_extract<int, int>()) {
+        buffer << socket.receive(512).get();
+        while (auto package = buffer.try_extract<int, int>()) {
             auto const [x, y] = package.value();
             std::cout << x << ',' << y << '\n';
         }
